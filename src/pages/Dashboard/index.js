cb8c2025ec690ba87f5db3d5228a3d6b47f4a560
Felipe Bergamin <felipebergamin6@gmail.com>
2019-10-02 15:21:05 -0300

    navegando entre os dias

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { format, subDays, addDays } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
@@ -6,18 +8,33 @@ import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 import { Container, Time } from './styles';
 
-export default function dashboard() {
+export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: ptBR }),
+    [date]
+  );
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
   return (
     <Container>
       <header>
         <button type="button">
-          <MdChevronLeft size={36} color="#fff" />
+          <MdChevronLeft size={36} color="#fff" onClick={handlePrevDay} />
         </button>
 
-        <strong>31 de maio</strong>
+        <strong>{dateFormatted}</strong>
 
         <button type="button">
-          <MdChevronRight size={36} color="#fff" />
+          <MdChevronRight size={36} color="#fff" onClick={handleNextDay} />
         </button>
       </header>
 

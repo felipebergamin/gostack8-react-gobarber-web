fb756bfd4ca53e6508a92609b1d96058221cca43
Felipe Bergamin <felipebergamin6@gmail.com>
2019-10-02 10:48:30 -0300

    add estilo ao componente Profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
-export default function profile() {
-  return <div />;
+export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha antiga"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirme a nova senha"
+        />
+
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }

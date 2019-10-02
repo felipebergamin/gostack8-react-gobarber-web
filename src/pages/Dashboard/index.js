da589bde335121e9b7a04a55a8a260b5e32efd22
Felipe Bergamin <felipebergamin6@gmail.com>
2019-10-02 15:06:27 -0300

    estilizando dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,47 @@
 import React from 'react';
 
-import api from '~/services/api';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+// import api from '~/services/api';
+
+import { Container, Time } from './styles';
 
 export default function dashboard() {
-  api.get('appointments');
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
 
-  return <h1>Dashboard</h1>;
+        <strong>31 de maio</strong>
+
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Felipe Bergamin</span>
+        </Time>
+
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+
+        <Time>
+          <strong>10:00</strong>
+          <span>Felipe Bergamin</span>
+        </Time>
+
+        <Time>
+          <strong>11:00</strong>
+          <span>Felipe Bergamin</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
